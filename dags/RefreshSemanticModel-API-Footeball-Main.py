9b71abf8e917f973e385c173ba1ac811a9b7fd19
Pedro <git-commit.r2ppz@mail.11111753.xyz>
2024-06-30 16:19:29 +0100

    Update and rename dasdasdas.py to RefreshSemanticModel-API-Footeball-Main.py

--- dags/RefreshSemanticModel-API-Footeball-Main.py
+++ dags/RefreshSemanticModel-API-Footeball-Main.py
@@ -5,17 +5,18 @@ from airflow.operators.bash import BashOperator
 from airflow_powerbi_plugin.operators.powerbi import PowerBIDatasetRefreshOperator
 
 with DAG(
-        dag_id='refresh_dataset_powerbi',
+        dag_id='RefreshPBISemanticModel-API-Footeball-Main',
         schedule_interval=None,
         start_date=datetime(2023, 8, 7),
         catchup=False,
         concurrency=20,
+        tags=["Public Projects","Refresh Semantic Model","API Footeball"],
 ) as dag:
 
     # [START howto_operator_powerbi_refresh_dataset]
     dataset_refresh = PowerBIDatasetRefreshOperator(
         powerbi_conn_id= "PowerBI_Default",
-        task_id="sync_dataset_refresh",
+        task_id="RefreshSemanticModel-API-Footeball-Main",
         dataset_id="1c808e6d-11e4-4a40-952c-f7d8952b2429",
         group_id="31d140f3-20e4-4365-97ab-15e1348d6dea",
     )
