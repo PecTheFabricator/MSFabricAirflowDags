c79b273223c0c5a405d33926f7f1c88ae231368d
Pedro <git-commit.r2ppz@mail.11111753.xyz>
2024-06-30 16:53:02 +0100

    Update RunFabricItems.py

--- dags/DataEngineeringProject/RunFabricItems.py
+++ dags/DataEngineeringProject/RunFabricItems.py
@@ -43,7 +43,7 @@ AzCostManage_Transformation_dag = SimpleHttpOperator(
     http_conn_id='http_ms_fabric',  # Replace with your connection ID
     endpoint='workspaces/31d140f3-20e4-4365-97ab-15e1348d6dea/items/ccd4ed3e-30ac-4ff8-87c6-a65b4483f833/jobs/instances?jobType=DefaultJob',  # Replace with your endpoint
     method='POST',
-    headers={"Content-Type": "application/json", "Authorization": "Bearer {{ var.value.auth_token }}"},
+    headers={"Content-Type": "application/json", "Authorization": "Bearer {{ var.value.AzAppAuthToken}}"},
     data=json.dumps({}),  # Replace with your data payload
     response_check=lambda response: response.status_code == 200,
     log_response=True,
