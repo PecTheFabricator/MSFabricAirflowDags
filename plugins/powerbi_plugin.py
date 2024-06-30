a6ff661e711433a325fd0c6c72114a45f7b21d36
Pedro <git-commit.r2ppz@mail.11111753.xyz>
2024-06-30 05:06:39 +0100

    Create powerbi_plugin.py

---
+++ plugins/powerbi_plugin.py
@@ -0,0 +1,18 @@
+from airflow.plugins_manager import AirflowPlugin
+
+from airflow_powerbi_plugin.hooks.powerbi import PowerBIHook
+from airflow_powerbi_plugin.operators.powerbi import PowerBILink
+
+# Defining the plugin class
+class AirflowExtraLinkPlugin(AirflowPlugin):
+    """
+    PowerBI plugin.
+    """
+
+    name = "powerbi_plugin"
+    operator_extra_links = [
+        PowerBILink(),
+    ]
+    hooks= [
+        PowerBIHook,
+    ]
